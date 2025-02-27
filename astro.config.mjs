3e4b12a16e8604d5e72697e5c57f96208f6e1b61
Antti Hanhela <antti@hanhela.org>
2025-02-27 18:26:11 +0000

    added assets, youtube embedded video for landing page with custom.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,24 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	
 	server: { host: true},
 	integrations: [
 		starlight({
-			title: 'Kubernetes 101',
+			title: '',
+			logo: {
+				src: './src/assets/kubernetes-horizontal-all-blue-color.png',
+			},
 			customCss: [
 				'./src/styles/custom.css',
 			],
 
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				facebook: 'https://www.facebook.com/',
+				github: 'https://github.com/',
+				rss: 'https://rss.com',
+				'x.com': 'https://www.x.com/'
+
 			},
 			sidebar: [
 
--- src/content/docs/arkkitehtuuri/yleiskuvaus.md
+++ src/content/docs/arkkitehtuuri/yleiskuvaus.md
@@ -3,6 +3,11 @@ sidebar:
     order: 1
 title: 'Yleiskuvaus'
 author: 'Antti'
+setup: |
+  import { Picture } from "astro-imagetools/components";
 ---
+
+![A local image](../../../assets/components-of-kubernetes.svg)
+
 
 Kubernetes-arkkitehtuuri rakentuu **control plane** -komponenteista (hallintakerros) ja **työntekijäsolmuista** (worker nodes). Control plane vastaa klusterin tilan hallinnasta ja sovelluskokonaisuuksien ohjaamisesta, kun taas työntekijäsolmut ajavat varsinaisia konteissa toimivia sovelluksia.
